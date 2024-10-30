784256eea5f7399606a59bdbd70a99c70685c750
Theodor Thornhill <theo@thornhill.no>
2024-10-30 22:42:22 +0100

    Take a buffer

--- src/typeid.zig
+++ src/typeid.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const base32 = @import("base32.zig");
 
-pub fn typeid(comptime prefix: []const u8, suffix: []const u8) ![]const u8 {
+pub fn typeid(comptime prefix: []const u8, suffix: []const u8, buf: *[99]u8) !usize {
     if (prefix.len > 63) {
         @compileError("Invalid length for prefix");
     }
@@ -12,9 +12,19 @@ pub fn typeid(comptime prefix: []const u8, suffix: []const u8) ![]const u8 {
         return error.InvalidCharacter;
     }
 
+    if (prefix[prefix.len-1] == '_') {
+        return error.InvalidCharacter;
+    }
+
     const encoded_suffix = (try base32.encode(suffix))[0..];
 
-    return prefix ++ "_" ++ encoded_suffix;
+    var len: usize = 0;
+    for (prefix ++ "_" ++ encoded_suffix, 0..) |c, i| {
+        buf[i] = c;
+        len = i;
+    }
+    len += 1;
+    return len;
 }
 
 const max_prefix_length = 63;
@@ -48,25 +58,33 @@ pub fn parts(id: []const u8) !Parts {
 }
 
 test "typeid encoding/decoding" {
+    var buf: [99]u8 = undefined;
+    var len = try typeid("foo", "01889c89-df6b-7f1c-a388-91396ec314bc", &buf);
     try std.testing.expectEqualStrings(
         "foo_01h2e8kqvbfwea724h75qc655w",
-        try typeid("foo", "01889c89-df6b-7f1c-a388-91396ec314bc"),
+        buf[0..len],
     );
+    len = try typeid("foo", "fa7cbdde-3ba5-4cc9-9275-a4f2b2563a55", &buf);
     try std.testing.expectEqualStrings(
         "foo_7tfjyxwex59k4s4xd4yas5cejn",
-        try typeid("foo", "fa7cbdde-3ba5-4cc9-9275-a4f2b2563a55"),
+        buf[0..len],
     );
+    len = try typeid("bar", "01889c89-df6b-7f1c-a388-91396ec314bc", &buf);
     try std.testing.expectEqualStrings(
         "bar_01h2e8kqvbfwea724h75qc655w",
-        try typeid("bar", "01889c89-df6b-7f1c-a388-91396ec314bc"),
+        buf[0..len],
     );
     try std.testing.expectError(
         error.InvalidLength,
-        typeid("foo", "01889c89-df6b-7f1c-a388-1396ec314bc"),
+        typeid("foo", "01889c89-df6b-7f1c-a388-1396ec314bc", &buf),
     );
     try std.testing.expectError(
         error.InvalidCharacter,
-        typeid("_foo", "01889c89-df6b-7f1c-a388-1396ec314bc"),
+        typeid("_foo", "01889c89-df6b-7f1c-a388-1396ec314bc", &buf),
+    );
+    try std.testing.expectError(
+        error.InvalidCharacter,
+        typeid("foo_", "01889c89-df6b-7f1c-a388-91396ec314bc", &buf),
     );
 
     const p = try parts("foo_bar_baz_01h2e8kqvbfwea724h75qc655w");
